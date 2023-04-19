9dfbaad71dfdaf900386e9f497ad6a75ad568b30
Tint team <no-reply@google.com>
2023-04-19 16:12:55 +0000

    Import Tint changes from Dawn
    Changes:
      - 692846d0a07b7d5e5e923102b73e66444b54eb4d tint/resolver: Fix use after move by Ben Clayton <bclayton@google.com>
      - d026e13f48d34d327fbe073d57f86438d32a90fe Remove `SymbolTable::NameFor` by dan sinclair <dsinclair@chromium.org>
      - b353ebe752fa0ba625e342e8cf92bd777e805a91 Add an allocator to store the symbol names. by dan sinclair <dsinclair@chromium.org>
      - fa3c08d112bd138cb396df64f205366a98bae6f8 Add a bump allocator. by dan sinclair <dsinclair@chromium.org>
    GitOrigin-RevId: 692846d0a07b7d5e5e923102b73e66444b54eb4d
    Change-Id: I111abe1eca4c5143391d0244531c65079543e81c
    Reviewed-on: https://dawn-review.googlesource.com/c/tint/+/128000
    Reviewed-by: Ben Clayton <bclayton@google.com>
    Commit-Queue: Ben Clayton <bclayton@google.com>
    Kokoro: Kokoro <noreply+kokoro@google.com>

--- src/tint/clone_context.cc
+++ src/tint/clone_context.cc
@@ -33,7 +33,7 @@ CloneContext::CloneContext(ProgramBuilder* to, Program const* from, bool auto_cl
         // Almost all transforms will want to clone all symbols before doing any
         // work, to avoid any newly created symbols clashing with existing symbols
         // in the source program and causing them to be renamed.
-        from->Symbols().Foreach([&](Symbol s, const std::string&) { Clone(s); });
+        from->Symbols().Foreach([&](Symbol s) { Clone(s); });
     }
 }
 
@@ -49,7 +49,7 @@ Symbol CloneContext::Clone(Symbol s) {
         if (symbol_transform_) {
             return symbol_transform_(s);
         }
-        return dst->Symbols().New(src->Symbols().NameFor(s));
+        return dst->Symbols().New(s.Name());
     });
 }
 
