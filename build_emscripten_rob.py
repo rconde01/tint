7109eea4a27dcba5c6e4a38c40ac9008b6612c3a
Rob Conde <Rob.Conde@ai-solutions.com>
2023-04-30 11:47:27 -0400

    update cmake options

--- build_emscripten_rob.py
+++ build_emscripten_rob.py
@@ -17,10 +17,10 @@ if __name__ == "__main__":
     os.environ["PATH"] = f'{depot_tools_path};{os.environ["PATH"]}'
     os.environ["DEPOT_TOOLS_WIN_TOOLCHAIN"] = "0"
 
-    ninja_unix = os.path.join(depot_tools_path,"ninja")
+    ninja_unix = os.path.join(depot_tools_path, "ninja")
 
     if os.path.exists(ninja_unix):
-        os.rename(ninja_unix,os.path.join(depot_tools_path,"ninja.unix"))
+        os.rename(ninja_unix, os.path.join(depot_tools_path, "ninja.unix"))
 
     gclient_file_template = os.path.join(script_dir, "standalone.gclient")
     gclient_file = os.path.join(script_dir, ".gclient")
@@ -40,8 +40,20 @@ if __name__ == "__main__":
         os.chdir(cmake_binary_dir)
 
         subprocess.check_call(
-            f"{emsdk_path}/emsdk_env.bat && emcmake cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release", shell=True)
-        
+            f"{emsdk_path}/emsdk_env.bat && " +
+            "emcmake cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release " +
+            "-DTINT_BUILD_SAMPLES=OFF " +
+            "-DTINT_BUILD_DOCS=OFF " +
+            "-DTINT_BUILD_SPV_READER=OFF " +
+            "-DTINT_BUILD_WGSL_READER=ON " +
+            "-DTINT_BUILD_GLSL_WRITER=OFF " +
+            "-DTINT_BUILD_HLSL_WRITER=OFF " +
+            "-DTINT_BUILD_MSL_WRITER=OFF " +
+            "-DTINT_BUILD_SPV_WRITER=OFF " +
+            "-DTINT_BUILD_WGSL_WRITER=OFF " +
+            "-DTINT_BUILD_SYNTAX_TREE_WRITER= ON " +
+            "-DTINT_BUILD_TESTS=OFF ", shell=True)
+
         subprocess.check_call(
             f"{emsdk_path}/emsdk_env.bat && cmake --build .", shell=True)
     finally:
