6e4e22bf671afecbf255481f0c81202d9e3f5645
Rob Conde <Rob.Conde@ai-solutions.com>
2023-05-07 15:15:34 -0400

    not working atm...i think i want to use a transformer

--- src/tint/wgsl-debugger/get_wgsl.cpp
+++ src/tint/wgsl-debugger/get_wgsl.cpp
@@ -6,6 +6,20 @@
 #include "src/tint/reader/wgsl/parser.h"
 #include "src/tint/source.h"
 #include "src/tint/writer/wgsl/generator.h"  // nogncheck
+#include "src/tint/clone_context.h"
+#include "src/tint/program_builder.h"
+#include "src/tint/utils/block_allocator.h"
+
+// from clone_context_test.cc
+// struct Allocator {
+//     template <typename T, typename... ARGS>
+//     T* Create(ARGS&&... args) {
+//         return alloc.Create<T>(this, std::forward<ARGS>(args)...);
+//     }
+
+//   private:
+//     tint::utils::BlockAllocator<tint::Cloneable> alloc;
+// };
 
 auto get_wgsl(std::string const& program_contents) -> std::string {
   auto source_file = std::make_unique<tint::Source::File>(
@@ -14,9 +28,34 @@ auto get_wgsl(std::string const& program_contents) -> std::string {
   auto program = std::make_unique<tint::Program>(
       tint::reader::wgsl::Parse(source_file.get()));
 
+  auto builder = tint::ProgramBuilder();
+
+  tint::CloneContext ctx(&builder,program.get());
+
+  auto functions = program->AST().Functions();
+
+  for(auto f : functions){
+    if(f->IsEntryPoint()){
+        for(auto s : f->body->statements){
+            ctx.InsertAfter(
+                f->body->statements,
+                s,
+                builder.Var(
+                    "wgsl_dgb_",
+                    tint::ast::Type{ builder.Expr(builder.Ident("f32"))}
+                )
+            );
+        }
+    }
+  }
+
+  ctx.Clone();
+
+  auto new_program = tint::Program(std::move(builder));
+
   tint::writer::wgsl::Options gen_options;
 
-  auto result = tint::writer::wgsl::Generate(program.get(), gen_options);
+  auto result = tint::writer::wgsl::Generate(&new_program, gen_options);
   if (!result.success) {
     std::ostringstream o;
 
